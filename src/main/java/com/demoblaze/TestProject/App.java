5cac76dd92d379dfc163c922172afea419eeb18e
way2atit <way2atit@gmail.com>
2022-06-01 21:16:13 +0530

    first Commit

---
+++ src/main/java/com/demoblaze/TestProject/App.java
@@ -0,0 +1,13 @@
+package com.demoblaze.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
